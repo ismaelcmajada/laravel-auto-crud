7bb9162695bb790fdb7ec7db1a8944f8a7d37b6e
ismaelcmajada <ismaelcmajada@gmail.com>
2024-12-19 09:10:25 +0000

    Feature: Added capability to fetch and display external relations in AutoExternalRelation component for dynamic data handling

--- resources/js/Components/LaravelAutoCrud/AutoExternalRelation.vue
+++ resources/js/Components/LaravelAutoCrud/AutoExternalRelation.vue
@@ -34,6 +34,20 @@ const storeExternalShortcutShow = ref(false)
 const pivotEditData = ref({})
 const pivotEditing = ref(null)
 
+const relations = ref({})
+
+const getRelations = () => {
+  const relationsFromFormFields = props.externalRelation.pivotFields?.filter(
+    (field) => field.relation
+  )
+
+  relationsFromFormFields.forEach((field) => {
+    axios.get(`${field.relation.endPoint}/all`).then((response) => {
+      relations.value[field.field] = response.data
+    })
+  })
+}
+
 const item = ref(props.item)
 
 const getItems = async () => {
@@ -119,6 +133,9 @@ const removeItem = (value) => {
 }
 
 getItems()
+if (props.externalRelation.pivotFields) {
+  getRelations()
+}
 </script>
 
 <template>
@@ -307,6 +324,7 @@ getItems()
       >
         <v-text-field
           v-if="
+            !field.relation &&
             field.type !== 'boolean' &&
             field.type !== 'date' &&
             field.type !== 'password' &&
@@ -362,6 +380,33 @@ getItems()
           v-model="pivotData[field.field]"
           :rules="getFieldRules(pivotData[field.field], field)"
         ></v-textarea>
+
+        <v-autocomplete
+          v-else-if="field.relation"
+          :items="
+            props.filteredItems[props.externalRelation.relation]
+              ? props.filteredItems[props.externalRelation.relation](
+                  relations[field.field]
+                )
+              : relations[field.field]
+          "
+          :label="field.rules?.required ? field.name + ' *' : field.name"
+          :item-props="props.customItemProps?.[field.relation.relation]"
+          :item-title="generateItemTitle(field.relation.formKey)"
+          :custom-filter="
+            (item, queryText, itemText) =>
+              searchByWords(
+                item,
+                queryText,
+                itemText,
+                props.customFilters?.[field.relation.relation]
+              )
+          "
+          item-value="id"
+          v-model="pivotData[field.field]"
+          :rules="getFieldRules(pivotData[field.field], field)"
+        >
+        </v-autocomplete>
       </v-col>
       <v-col
         v-if="props.externalRelation.pivotFields"
@@ -471,6 +516,7 @@ getItems()
         >
           <v-text-field
             v-if="
+              !field.relation &&
               field.type !== 'boolean' &&
               field.type !== 'date' &&
               field.type !== 'password' &&
@@ -527,6 +573,33 @@ getItems()
             v-model="pivotEditData[field.field]"
             :rules="getFieldRules(pivotEditData[field.field], field)"
           ></v-textarea>
+
+          <v-autocomplete
+            v-else-if="field.relation"
+            :items="
+              props.filteredItems?.[field.relation.relation]
+                ? props.filteredItems[field.relation.relation](
+                    relations[field.field]
+                  )
+                : relations[field.field]
+            "
+            :label="field.rules?.required ? field.name + ' *' : field.name"
+            :item-props="props.customItemProps?.[field.relation.relation]"
+            :item-title="generateItemTitle(field.relation.formKey)"
+            :custom-filter="
+              (item, queryText, itemText) =>
+                searchByWords(
+                  item,
+                  queryText,
+                  itemText,
+                  props.customFilters?.[field.relation.relation]
+                )
+            "
+            item-value="id"
+            v-model="pivotEditData[field.field]"
+            :rules="getFieldRules(pivotEditData[field.field], field)"
+          >
+          </v-autocomplete>
         </v-col>
         <v-col cols="12" class="text-center">
           <v-btn
